6ca07adeb2ccace7d13f54bf73df393f6549b85a
tdaw820 <yggtorrent-tdaw820@sinux.sh>
2025-12-21 18:26:49 +0100

    feat: YGG env var

--- init-indexers.sh
+++ init-indexers.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+YGG="${YGG:-download}"
+
 mkdir -p /config/Definitions/Custom/
 cp /app/indexers/*.yml /config/Definitions/Custom/
 
 chown -R abc:abc /config/Definitions/Custom
 chmod -R 644 /config/Definitions/Custom/*.yml
+
+if [ "$YGG" = "magnet" ]; then
+    rm -f /config/Definitions/Custom/ygg-api-download.yml
+else
+    rm -f /config/Definitions/Custom/ygg-api-magnet.yml
+fi
 
 # Execute the original entrypoint with all arguments
 exec /init-linuxserver "$@"
